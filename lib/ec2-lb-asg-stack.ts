37a1f2154a35ff07c5287662d8ad097ed1d2b1b3
jayparmar <infinitie.jay@gmail.com>
2025-01-30 10:16:57 +0530

    changes

--- lib/ec2-lb-asg-stack.ts
+++ lib/ec2-lb-asg-stack.ts
@@ -4,6 +4,9 @@ import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 
 
 export class Ec2LbAsgStack extends cdk.Stack {
@@ -38,7 +41,7 @@ export class Ec2LbAsgStack extends cdk.Stack {
       engine: rds.DatabaseInstanceEngine.mysql({
         version: rds.MysqlEngineVersion.VER_8_0_34,
       }),
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.LARGE),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
       credentials: rds.Credentials.fromGeneratedSecret('admin'),
       databaseName: 'codeigniter',
       vpc,
@@ -55,7 +58,7 @@ export class Ec2LbAsgStack extends cdk.Stack {
         'apt-get install -y software-properties-common',
         'add-apt-repository ppa:ondrej/php -y',
         'apt-get update -y',
-        'apt-get install -y php8.0 php8.0-mysql apache2 git',
+        'apt-get install -y php8.0 php8.0-mysql php8.0-curl apache2 git',
         'systemctl start apache2',
         'systemctl enable apache2',
         'rm -rf /var/www/html',
@@ -70,10 +73,7 @@ export class Ec2LbAsgStack extends cdk.Stack {
         'echo "    AllowOverride All" >> /etc/apache2/conf-available/allow-htaccess.conf',
         'echo "</Directory>" >> /etc/apache2/conf-available/allow-htaccess.conf',
         'a2enconf allow-htaccess',
-        'systemctl reload apache2',
-        `echo "export DB_HOST=${database.dbInstanceEndpointAddress}" >> /etc/environment`,
-        `echo "export DB_USER=admin" >> /etc/environment`,
-        `echo "export DB_PASS=$(aws secretsmanager get-secret-value --secret-id '${database.secret?.secretArn}' --query SecretString --output text | jq -r .password)" >> /etc/environment`
+        'systemctl reload apache2'
     );
 
     // Step 5: Create Auto Scaling Group
@@ -120,7 +120,126 @@ export class Ec2LbAsgStack extends cdk.Stack {
     });
 
 
+
+    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+        comment: id + ' CloudFront Distribution for main site',
+      defaultBehavior: {
+        origin: new origins.LoadBalancerV2Origin(alb, {
+          protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+        }),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED, // Disable caching for API requests
+        originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER, // Forward all headers
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL, // Allow all HTTP methods
+      },
+      additionalBehaviors: {
+        '*.css': {
+          origin: new origins.LoadBalancerV2Origin(alb, {
+            protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+          }),
+          viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+          cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+        },
+        '*.js': {
+          origin: new origins.LoadBalancerV2Origin(alb, {
+            protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+          }),
+          viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+          cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+        },
+      },
+    });
+
+      const apiDistribution = new cloudfront.Distribution(this, 'API-Distribution', {
+          comment: id + ' CloudFront Distribution for api',
+          defaultBehavior: {
+              origin: new origins.LoadBalancerV2Origin(alb, {
+                  protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+              }),
+              viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+              cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED, // Disable caching for API requests
+              originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER, // Forward all headers
+              allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL, // Allow all HTTP methods
+          },
+          additionalBehaviors: {
+              '*.css': {
+                  origin: new origins.LoadBalancerV2Origin(alb, {
+                      protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+                  }),
+                  viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                  cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+              },
+              '*.js': {
+                  origin: new origins.LoadBalancerV2Origin(alb, {
+                      protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+                  }),
+                  viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                  cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+              },
+          },
+      });
+
+      const merchantDistribution = new cloudfront.Distribution(this, 'merchant-Distribution', {
+          comment: id + ' CloudFront Distribution for merchant',
+          defaultBehavior: {
+              origin: new origins.LoadBalancerV2Origin(alb, {
+                  protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+              }),
+              viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+              cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED, // Disable caching for API requests
+              originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER, // Forward all headers
+              allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL, // Allow all HTTP methods
+          },
+          additionalBehaviors: {
+              '*.css': {
+                  origin: new origins.LoadBalancerV2Origin(alb, {
+                      protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+                  }),
+                  viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                  cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+              },
+              '*.js': {
+                  origin: new origins.LoadBalancerV2Origin(alb, {
+                      protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+                  }),
+                  viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                  cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+              },
+          },
+      });
+
+      const adminDistribution = new cloudfront.Distribution(this, 'admin-Distribution', {
+          comment: id + ' CloudFront Distribution for admin',
+          defaultBehavior: {
+              origin: new origins.LoadBalancerV2Origin(alb, {
+                  protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+              }),
+              viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+              cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED, // Disable caching for API requests
+              originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER, // Forward all headers
+              allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL, // Allow all HTTP methods
+          },
+          additionalBehaviors: {
+              '*.css': {
+                  origin: new origins.LoadBalancerV2Origin(alb, {
+                      protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+                  }),
+                  viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                  cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+              },
+              '*.js': {
+                  origin: new origins.LoadBalancerV2Origin(alb, {
+                      protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+                  }),
+                  viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                  cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+              },
+          },
+      });
     // Outputs
+    new cdk.CfnOutput(this, 'CloudFrontURL', { value: distribution.distributionDomainName });
+
+    // Output
     new cdk.CfnOutput(this, 'ALBDNS', { value: alb.loadBalancerDnsName });
     new cdk.CfnOutput(this, 'DBEndpoint', { value: database.dbInstanceEndpointAddress });
   }
